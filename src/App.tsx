64d6370909d5f08f4fde144c74d31d4f458ca18a
zem <zem@qq.com>
2022-04-16 22:14:05 +0800

    feat: txtForSearch; isSpeaking;

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,57 @@ import { useState, MouseEvent } from 'react'
 import './App.css'
 import TXTa from '../txt/2'
 import TXTb from '../txt/诡秘之主.js'
-const TXT2 = (() => {
-  console.log(TXTa.length)
-  if ('111111111'.length % 2) {
-    return TXTa.replaceAll('\n\n', '\n    ').slice(0, 2e4)
+import TXTc from '../txt/jy/sd'
+const flag = '111'.length % 2
+const txt = (() => {
+  if (flag) {
+    return TXTc.replaceAll('\n', '\n\n').slice(0, 111)
   } else {
-    return TXTb.slice(0, 300).replaceAll('\n', '    ')
+    return TXTb.slice(0, 1e6).replaceAll('\n', '    ')
   }
 })()
-const txt = (() => {
-  // console.time('txt')
-  const txt = (
-    <div>
-      {[...TXT2].map((e, i, arr) => {
-        if (i < arr.length / 211) {
-          return (
-            <i key={i} data-e={e} data-i={i} className={i as unknown as string}>
-              {e}
-            </i>
-          )
-        }
+const txtForSearch = (() => {
+  if (flag) {
+    console.log(TXTc.length)
+    return TXTc
+  } else {
+    console.log(TXTb.length)
+    return TXTb
+  }
+})()
 
-        return (
-          <i key={i} data-e={e} data-i={i} className={i as unknown as string}>
-            {e}
-          </i>
-        )
-        if (i < arr.length / 2) {
-          return (
-            <i
-              key={i}
-              data-el={e}
-              data-i={i}
-              className={i as unknown as string}
-            >
-              {e}
-            </i>
-          )
-        }
-        return (
-          <i key={i} data-er={e} data-i={i} className={i as unknown as string}>
-            {e}
-          </i>
-        )
-      })}
-    </div>
-  )
-  // console.timeEnd('txt')
-  return txt
+const txtDom = (() => {
+  // console.time('txtDom')
+  let isSpeaking: boolean
+  const txtDom = [...txt].map((e, i, arr) => {
+    if (e === '”') isSpeaking = false
+    const dom = (
+      <i
+        key={i}
+        data-e={e}
+        data-i={i}
+        className={isSpeaking && e != '　' ? 'isSpeaking' : ''}
+      >
+        {e}
+      </i>
+    )
+    if (e === '“') isSpeaking = true
+    return dom
+    if (i < arr.length / 2) {
+      return (
+        <i key={i} data-el={e} data-i={i} className={i as unknown as string}>
+          {e}
+        </i>
+      )
+    }
+    return (
+      <i key={i} data-er={e} data-i={i} className={i as unknown as string}>
+        {e}
+      </i>
+    )
+  })
+  // console.timeEnd('txtDom')
+  return txtDom
 })()
 
 const colors = ['red', 'blueviolet', 'gainsboro', 'magenta']
@@ -79,17 +82,23 @@ export default function App() {
           value={select}
           onChange={e => setSelect(e.target.value)}
         />
-        -- {TXT2.split(select).length - 1} --
+        -- {txtForSearch.split(select).length - 1} --
         <input type='text' value={s} onChange={e => ss(e.target.value)} />
       </div>
 
-      <div className='wrap' onClick={e => nextDom(e, setSelect, select)}>
-        <div>年</div>
-        <div>年</div>
-        <div>年</div>
-        <div>年</div>
+      <div className='wrap'>
         <div>样赞叹这座二百五十年前的建筑物</div>
-        {txt}
+        <div>样赞叹这座二百五十年前的建筑物</div>
+        <div
+          onClick={e => nextDom(e, setSelect, select)}
+          onDoubleClick={e => {
+            e.stopPropagation()
+            e.preventDefault()
+            return false
+          }}
+        >
+          {txtDom}
+        </div>
       </div>
     </>
   )
@@ -109,22 +118,23 @@ function nextDom(
   const selectionStr = selectionObj.toString()
   if (selectionStr) {
     setSelect(selectionStr)
-    selectionObj.removeAllRanges()
+    // selectionObj.removeAllRanges()
     return
   }
 
   if (!(target instanceof HTMLElement)) return
-  const itemIdx = searchWordCount(select).flatMap(n =>
-    Array(select.length)
-      .fill(0)
-      .map((_, i) => n + i)
-  )
 
-  const currentIdx = itemIdx.indexOf(Number(target.dataset.i))
-  if (currentIdx === -1) return
+  const wordAllPosition = getWordAllPosition(select)
 
-  const distance = select.length * (nativeEvent.metaKey ? -1 : 1)
-  const targetDomIdx = itemIdx[currentIdx + distance]
+  const clickIdx = wordAllPosition.indexOf(Number(target.dataset.i))
+  if (clickIdx === -1) return
+
+  let nextIdx = clickIdx + select.length * (nativeEvent.metaKey ? -1 : 1)
+  if (nextIdx > wordAllPosition.length - 1) {
+    //处理从数组尾到头
+    nextIdx = nextIdx - wordAllPosition.length
+  }
+  const targetDomIdx = wordAllPosition.at(nextIdx) //处理头到尾
   const targetDom = document.querySelector<HTMLElement>(
     `[data-i='${targetDomIdx}']`
   )
@@ -134,20 +144,26 @@ function nextDom(
 }
 
 // 查找一个字符串中的所有子串的位置
-function searchWordCount(word: string) {
-  // if (word === '') return [] //
+function getWordPosition(word: string) {
   const positions = []
-  let pos = TXT2.indexOf(word)
+  let pos = txt.indexOf(word)
   while (pos > -1) {
     positions.push(pos)
-    pos = TXT2.indexOf(word, pos + word.length)
+    pos = txt.indexOf(word, pos + word.length)
   }
   return positions
+}
+function getWordAllPosition(word: string) {
+  return getWordPosition(word).flatMap(n =>
+    Array(word.length)
+      .fill(0)
+      .map((_, i) => n + i)
+  )
 }
 
 function getCssSelectorL(select: string) {
   const len = select.length
-  const count = TXT2.split(select).length - 1
+  const count = txt.split(select).length - 1
   if (count === 0) return
 
   return (
@@ -169,7 +185,7 @@ function getCssSelectorL(select: string) {
 }
 function getCssStyleR(word: string, idx: number) {
   const css1 =
-    searchWordCount(word).length === 1
+    txtForSearch.split(word).length - 1 === 1
       ? 'text-decoration: line-through red'
       : 'cursor:se-resize'
   const css2 = `color:${colors[idx] || 'black'}`
